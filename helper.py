b8d1437868b8f27fae9db63dfd690af244f15bb6
adunlop1234 <dunlop.arthur@gmail.com>
2021-11-30 18:52:12 +0000

    Initial commit.

---
+++ helper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Function to read in the lines of the input file
+def get_input_lines(filename):
+    with open(filename, "r") as f:
+        lines = f.read().splitlines()
+        return lines
---
+++ template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Import helper functions
+import functools
+
+# Specify the filename
+VERSION = 1
+EXAMPLE_FILENAME = str(VERSION) + "_EX.txt"
+PROBLEM_FILENAME = str(VERSION) + ".txt"
+
+def main():
+    
+    # Get the input lines for example and full problem
+    example_lines = helper.get_input_lines(EXAMPLE_FILENAME)
+    problem_lines = helper.get_input_lines(PROBLEM_FILENAME)
+
+    # Perform function one on example and input lines
+    part_one(example_lines)
+    part_one(problem_lines)
+
+    # Perform function two on example and input lines
+    part_two(example_lines)
+    part_two(problem_lines)
+
+def print_result(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        print("Output of " + func.__name__ + " is: " + str(output))
+        return output
+    return wrapper
+
+# Function to read in the lines of the input file
+def get_input_lines(filename):
+    with open(filename, "r") as f:
+        lines = f.read().splitlines()
+        return lines
+
+@print_result
+def part_one(lines):
+    pass
+
+@print_result
+def part_two(lines):
+    pass
+
+if __name__ == "__main__":
+    main()
