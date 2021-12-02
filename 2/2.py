4335e27a4e380053ff3e89388b737ba749b9be60
adunlop1234 <dunlop.arthur@gmail.com>
2021-12-02 09:05:19 +0000

    Completed day 2.

--- 2/2.py
+++ 2/2.py
@@ -43,11 +43,39 @@ def get_input_lines(filename):
 
 @print_result
 def part_one(lines):
-    pass
+    
+    depth = 0
+    horizontal = 0
+    for line in lines:
+        command, value = line.split(' ')
+
+        if command == 'forward':
+            horizontal += int(value)
+        elif command == 'down':
+            depth += int(value)
+        elif command == 'up':
+            depth -= int(value)
+
+    return depth * horizontal
 
 @print_result
 def part_two(lines):
-    pass
+
+    aim = 0
+    depth = 0
+    horizontal = 0
+    for line in lines:
+        command, value = line.split(' ')
+
+        if command == 'forward':
+            horizontal += int(value)
+            depth += int(value) * aim
+        elif command == 'down':
+            aim += int(value)
+        elif command == 'up':
+            aim -= int(value)
+
+    return depth * horizontal
 
 if __name__ == "__main__":
     main()
