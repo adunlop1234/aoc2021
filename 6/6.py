0db475190f4d705b75b96685852ae1373cd9d6c4
adunlop1234 <dunlop.arthur@gmail.com>
2021-12-06 12:48:29 +0000

    Complete day 6.

--- 6/6.py
+++ 6/6.py
@@ -42,13 +42,103 @@ def get_input_lines(filename):
         lines = f.read().splitlines()
         return lines
 
+class LanternFish():
+    def __init__(self, timer=8):
+        self.timer = timer
+
+    def decrease_timer(self):
+        self.timer -= 1
+
+    def check_new_fish_created(self):
+
+        if self.timer == -1:
+            self.timer = 6
+            return True
+
+        return False
+
+    def __str__(self):
+        return "Fish with timer of: " + str(self.timer)
+
 @print_result
 def part_one(lines):
-    pass
+    
+    for line in lines:
+
+        # Get timers of the initial fish
+        timers = line.split(',')
+
+    # Initialise the fish
+    all_fish = []
+    for timer in timers:
+        all_fish.append(LanternFish(int(timer)))
+
+    # Loop over the number of days
+    days = 80
+    day = 0
+    while day < days:
+        new_fish = []
+        for fish in all_fish:
+            fish.decrease_timer()
+
+            if fish.check_new_fish_created():
+                new_fish.append(LanternFish())
+
+        all_fish += new_fish
+
+        day += 1
+        #print("End of day " + str(day) + ": Number of fish = " + str(len(all_fish)))
+
+    return len(all_fish)
+
 
 @print_result
 def part_two(lines):
-    pass
+    for line in lines:
+
+        # Get timers of the initial fish
+        timers = line.split(',')
+
+    # Initialise the fish into groups:
+    #   number on the timer
+    #   number of fish at this level
+    #   need 0-8 on the timer so 9 groups that have timer and number
+    #       struct:
+    #           'timer' : 'number'
+    
+    all_fish = {timer:number for timer, number in zip(range(0, 9), [0]*9)}
+
+    for timer in timers:
+        all_fish[int(timer)] += 1
+
+    # Loop over the number of days
+    days = 256
+    day = 0
+    while day < days:
+
+        new_all_fish = {timer:number for timer, number in zip(range(0, 9), [0]*9)}
+
+        # All the new fish that are born
+        new_all_fish[8] = all_fish[0]
+
+        # Decrease the timer
+        for i in range(1, 9):
+            new_all_fish[i-1] = all_fish[i]
+
+        # Add the old fish at 0 to 6
+        new_all_fish[6] += all_fish[0]
+
+        # Set the current fish to be all of the new ones
+        all_fish = new_all_fish.copy()
+
+        # Report after each day the calculation of total fish
+        total_fish = sum([number for number in all_fish.values()])
+
+        day += 1
+        #print("End of day " + str(day) + ": Number of fish = " + str(total_fish))
+
+    return total_fish
+
 
 if __name__ == "__main__":
     main()
