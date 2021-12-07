7ad9b4da11e8fcf4843e75e9bba61fe8ddc5089f
adunlop1234 <dunlop.arthur@gmail.com>
2021-12-07 20:21:00 +0000

    Day 7 completed.

--- 7/7.py
+++ 7/7.py
@@ -44,11 +44,41 @@ def get_input_lines(filename):
 
 @print_result
 def part_one(lines):
-    pass
+    
+    # Get the numbers
+    numbers = [int(number) for number in lines[0].split(',')]
+    
+    # Store in numpy array and sort
+    numbers = np.array(numbers).reshape(-1, 1)
+    best_option = int(np.median(numbers))
+    min_cost = int(np.absolute(numbers - best_option).sum())
+
+    return "Best option: " + str(int(best_option)) + ". Cost = " + str(int(min_cost))
 
 @print_result
 def part_two(lines):
-    pass
+
+    # Get the numbers
+    numbers = [int(number) for number in lines[0].split(',')]
+    options = range(0, max(numbers) + 1)
+
+     # Store in numpy array and sort
+    numbers = np.array(numbers).reshape(-1, 1)
+    totals = {float(n) : (n * (n + 1) * 0.5) for n in range(0, numbers.max() + 1)}
+
+    min_cost = 1000000000000000000000000000000
+    best_option = 0
+    for option in options:
+        deltas = np.absolute(numbers - option)
+        cost = 0
+        for delta in deltas[:, 0]:
+            cost += totals[float(delta)]
+
+        if cost < min_cost:
+            min_cost = min(cost, min_cost)
+            best_option = option
+
+    return "Best option: " + str(int(best_option)) + ". Cost = " + str(int(min_cost))
 
 if __name__ == "__main__":
     main()
