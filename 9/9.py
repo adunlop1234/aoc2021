d8cdcc5979ad1623c3dab80a72b6c0d1bc73417d
adunlop1234 <dunlop.arthur@gmail.com>
2021-12-08 21:45:18 +0000

    Prepared for day 9.

---
+++ 9/9.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+'''
+Template file for each new challenge.
+'''
+
+# Import helper functions
+import functools
+import os, sys
+import numpy as np
+
+# Specify the filename
+VERSION = os.getcwd().split(os.path.sep)[-1].split('.')[0]
+EXAMPLE_FILENAME = str(VERSION) + "_EX.txt"
+PROBLEM_FILENAME = str(VERSION) + ".txt"
+
+def main():
+    
+    # Get the input lines for example and full problem
+    example_lines = get_input_lines(EXAMPLE_FILENAME)
+    problem_lines = get_input_lines(PROBLEM_FILENAME)
+
+    # Perform function one on example and input lines
+    part_one(example_lines)
+    part_one(problem_lines)
+
+    # Perform function two on example and input lines
+    part_two(example_lines)
+    part_two(problem_lines)
+
+def print_result(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        print("Output of " + func.__name__ + " is: " + str(output))
+        return output
+    return wrapper
+
+# Function to read in the lines of the input file
+def get_input_lines(filename):
+    with open(filename, "r") as f:
+        lines = f.read().splitlines()
+        return lines
+
+@print_result
+def part_one(lines):
+    pass
+
+@print_result
+def part_two(lines):
+    pass
+
+if __name__ == "__main__":
+    main()
